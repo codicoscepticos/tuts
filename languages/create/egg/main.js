237930246fe42d086bc6bf4f9f1e7832b49ca4c5
codicoscepticos <bigdippergames@gmail.com>
2019-07-02 17:53:32 +0300

    create language: egg (updated main.js, comments)

--- languages/create/egg/main.js
+++ languages/create/egg/main.js
@@ -4,8 +4,56 @@ const wordStr = "word";
 
 const regexps = {
   string: /^"([^"]*)"/,
+  // #region String Regexp
+  /*
+  RegExp explanation:
+  ^ : matching the beginning of a string
+  " : matches a " character
+  () : groups multiple tokens (e.g. *hahaha*h *ha*a *ha*h)
+  [^] : negated set, matches any character that's not in the set
+  [^"] : matches any character that is not a "
+  [^"]* : matches 0 or more of the preceding token
+  " : matches a " character
+  ---
+  It matches the beginning of the string for a " character,
+  then it matches (0+ of) any no-" characters,
+  then it matches a (closing) " character.
+  So it matches a double-quoted string with content which does not contain
+  any double quote.
+  It matches only the first match and it doesn't match any subsequent
+  matches.
+  */
+  // #endregion String Regexp
   digit: /^\d+\b/,
+  // #region Digit Regexp
+  /*
+  RegExp explanation:
+  ^ : matching the beginning of a string
+  \d : matches any digit character
+  \d+ : match 1 or more of the preceding token
+  \b : matches a word boundary position between a word character and
+  non-word character or position (e.g. she sell*s* seashell*s*)
+  ---
+  It matches any didgit character(s), which is alone, e.g. a match: *123*
+  123, not a match: 123a 123. It matches only the first match and it
+  doesn't match any subsequent match.
+  */
+  // #endregion Digit Regexp
   word: /^[^\s(),#"]+/
+  // #region Word Regexp
+  /*
+  RegExp explanation:
+  ^ : matching the beginning of a string
+  [^] : negated set, matches any character that's not in the set
+  [^\s(), #"] : matches any character that's not a whitespace \s or a ( or
+  a ) or a , or a #
+  + : matches 1 or more of the preceding token
+  ---
+  It matches words (these are not strings, i.e. no content inside
+  double-quotes, but just plain words, i.e. a match: haha, not a match:
+  "haha").
+  */
+  // #endregion Word Regexp
 };
 const regexpsKeys = Object.keys(regexps);
 const regexpsValues = Object.values(regexps);
@@ -55,61 +103,10 @@ function parseExpression(program) {
   }
 
   if (exprType === stringStr) {
-    // #region String Regexp
-    /*
-    RegExp explanation:
-    ^ : matching the beginning of a string
-    " : matches a " character
-    () : groups multiple tokens (e.g. *hahaha*h *ha*a *ha*h)
-    [^] : negated set, matches any character that's not in the set
-    [^"] : matches any character that is not a "
-    [^"]* : matches 0 or more of the preceding token
-    " : matches a " character
-    ---
-    It matches the beginning of the string for a " character,
-    then it matches (0+ of) any no-" characters,
-    then it matches a (closing) " character.
-    So it matches a double-quoted string with content which does not contain
-    any double quote.
-    It matches only the first match and it doesn't match any subsequent
-    matches.
-    */
-    // #endregion String Regexp
-
     expr = { type: "value", value: match[1] };
   } else if (exprType === digitStr) {
-    // #region Digit Regexp
-    /*
-    RegExp explanation:
-    ^ : matching the beginning of a string
-    \d : matches any digit character
-    \d+ : match 1 or more of the preceding token
-    \b : matches a word boundary position between a word character and
-    non-word character or position (e.g. she sell*s* seashell*s*)
-    ---
-    It matches any didgit character(s), which is alone, e.g. a match: *123*
-    123, not a match: 123a 123. It matches only the first match and it
-    doesn't match any subsequent match.
-    */
-    // #endregion Digit Regexp
-
     expr = { type: "value", value: Number(match[0]) };
   } else if (exprType === wordStr) {
-    // #region Word Regexp
-    /*
-    RegExp explanation:
-    ^ : matching the beginning of a string
-    [^] : negated set, matches any character that's not in the set
-    [^\s(), #"] : matches any character that's not a whitespace \s or a ( or
-    a ) or a , or a #
-    + : matches 1 or more of the preceding token
-    ---
-    It matches words (these are not strings, i.e. no content inside
-    double-quotes, but just plain words, i.e. a match: haha, not a match:
-    "haha").
-    */
-    // #endregion Word Regexp
-
     expr = { type: wordStr, name: match[0] };
   } else {
     throw new SyntaxError(`Unexpected syntax: ${program}`);
